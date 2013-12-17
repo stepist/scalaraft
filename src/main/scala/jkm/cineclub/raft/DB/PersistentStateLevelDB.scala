f87ee18823af66b431f9971aa47d824f4a92c60d
cineclub <cineclub.kjm@gmail.com>
2013-12-17 23:23:19 +0900

    ...

--- src/main/scala/jkm/cineclub/raft/DB/PersistentStateLevelDB.scala
+++ src/main/scala/jkm/cineclub/raft/DB/PersistentStateLevelDB.scala
@@ -21,22 +21,18 @@ class PersistentStateLevelDB(val dbName:String,val dbRootPath:String=null) exten
   val db :DB = factory.open(new File(dbRootPath,dbName), new Options())
   implicit val writeOption=new WriteOptions().sync(true)
 
-  def batchPut[T: SPickler: FastTypeTag](batch:WriteBatch,item: PersistentStateItem[T]) =
-    batch.put( item.dbKey.getBytes,item.value.pickle.value)
-
-
 
   def set(ps: PersistentState) = {
     val batch :WriteBatch= db.createWriteBatch()
 
     try {
-      if (ps.lastAppendedIndex != null) batchPut(batch,ps.lastAppendedIndex)
-      if (ps.lastAppliedIndex != null) batchPut(batch,ps.lastAppliedIndex)
-      if (ps.leaderCommitIndex != null) batchPut(batch,ps.leaderCommitIndex)
-      if (ps.myId != null) batchPut(batch,ps.myId)
-      if (ps.raftMemberId != null) batchPut(batch,ps.raftMemberId)
-      if (ps.raftMembership != null) batchPut(batch,ps.raftMembership)
-      if (ps.termInfo != null) batchPut(batch,ps.termInfo)
+      if (ps.lastAppendedIndex != null) batch.put(LastAppendedIndexDBKey.getBytes, ps.lastAppendedIndex.pickle.value)
+      if (ps.lastAppliedIndex != null) batch.put(LastAppliedIndexDBKey.getBytes, ps.lastAppliedIndex.pickle.value)
+      if (ps.leaderCommitIndex != null) batch.put(LeaderCommitIndexDBKey.getBytes, ps.leaderCommitIndex.pickle.value)
+      if (ps.myId != null) batch.put(MyIdDBKey.getBytes, ps.myId.pickle.value)
+      if (ps.raftMemberId != null) batch.put(RaftMemberIdDBKey.getBytes, ps.raftMemberId.pickle.value)
+      if (ps.raftMembership != null) batch.put(RaftMembershipDBKey.getBytes, ps.raftMembership.pickle.value)
+      if (ps.termInfo != null) batch.put(TermInfoDBKey.getBytes, ps.termInfo.pickle.value)
 
       db.write(batch,writeOption)
     } finally {batch.close()}
@@ -46,16 +42,41 @@ class PersistentStateLevelDB(val dbName:String,val dbRootPath:String=null) exten
 
   //binary.toBinaryPickle(data).unpickle[T]
 
-  def get(ps: PersistentState): PersistentState = {
-    val a=RaftMemberIdWrap
+  def get(dbKeys:List[PersistentStateDBKey]): PersistentState = {
+    var lastAppendedIndex:LastAppendedIndex = -1
 
-    if (ps.lastAppendedIndex != null) binary.toBinaryPickle(db.get(LastAppendedIndexWrap().dbKey.getBytes())).unpickle[LastAppendedIndexWrap]
-    if (ps.lastAppliedIndex != null) batchPut(batch,ps.lastAppliedIndex)
-    if (ps.leaderCommitIndex != null) batchPut(batch,ps.leaderCommitIndex)
-    if (ps.myId != null) batchPut(batch,ps.myId)
-    if (ps.raftMemberId != null) batchPut(batch,ps.raftMemberId)
-    if (ps.raftMembership != null) batchPut(batch,ps.raftMembership)
-    if (ps.termInfo != null) batchPut(batch,ps.termInfo)
+    if (ps.lastAppendedIndex != null)  binary.toBinaryPickle(db.get(LastAppendedIndexDBKey.getBytes)).unpickle[LastAppendedIndex]
+    if (ps.lastAppliedIndex != null) binary.toBinaryPickle(db.get(LastAppliedIndexDBKey.getBytes)).unpickle[LastAppliedIndex]
+    if (ps.leaderCommitIndex != null) binary.toBinaryPickle(db.get(LeaderCommitIndexDBKey.getBytes)).unpickle[LeaderCommitIndex]
+    if (ps.myId != null) binary.toBinaryPickle(db.get(MyIdDBKey.getBytes)).unpickle[MyId]
+    if (ps.raftMemberId != null) binary.toBinaryPickle(db.get(RaftMemberIdDBKey.getBytes)).unpickle[RaftMemberId]
+    if (ps.raftMembership != null) binary.toBinaryPickle(db.get(RaftMembershipDBKey.getBytes)).unpickle[RaftMembership]
+    if (ps.termInfo != null) binary.toBinaryPickle(db.get(TermInfoDBKey.getBytes)).unpickle[TermInfo]
+
 
   }
+
+  def get2(dbKey:PersistentStateDBKey): PersistentState =
+    dbKey match{
+      case LastAppendedIndexDBKey => new PersistentState(lastAppendedIndex=binary.toBinaryPickle(db.get(LastAppendedIndexDBKey.getBytes)).unpickle[LastAppendedIndex] )
+      case LastAppliedIndexDBKey => new PersistentState( lastAppliedIndex= binary.toBinaryPickle(db.get(LastAppliedIndexDBKey.getBytes)).unpickle[LastAppliedIndex] )
+      case LeaderCommitIndexDBKey => new PersistentState( leaderCommitIndex = binary.toBinaryPickle(db.get(LeaderCommitIndexDBKey.getBytes)).unpickle[LeaderCommitIndex] )
+      case MyIdDBKey =>  new PersistentState(myId=  binary.toBinaryPickle(db.get(MyIdDBKey.getBytes)).unpickle[MyId] )
+      case RaftMemberIdDBKey =>  new PersistentState( raftMemberId= binary.toBinaryPickle(db.get(RaftMemberIdDBKey.getBytes)).unpickle[RaftMemberId] )
+      case RaftMembershipDBKey => new PersistentState( raftMembership =  binary.toBinaryPickle(db.get(RaftMembershipDBKey.getBytes)).unpickle[RaftMembership] )
+      case TermInfoDBKey => new PersistentState( termInfo =  binary.toBinaryPickle(db.get(TermInfoDBKey.getBytes)).unpickle[TermInfo] )
+    }
+}
+
+}
+
+
+object TestPersistentStateLevelDB extends App{
+  case class AA(a:Int)
+  case class BB(b:String) extends AA(3)
+
+  val c=BB("hello").pickle.value
+  val d= binary.toBinaryPickle(c).unpickle[AA]
+  println(d)
+
 }
--- src/main/scala/jkm/cineclub/raft/PersistentState.scala
+++ src/main/scala/jkm/cineclub/raft/PersistentState.scala
@@ -23,29 +23,29 @@ object PersistentState {
   type LastAppliedIndex=Long
   type LastAppendedIndex=Long
 
-  case class PersistentStateItem[T](value:T,dbKey:String)
 
-  case class RaftMemberIdWrap(override val value:RaftMemberId=null)  extends PersistentStateItem[RaftMemberId](value,"RaftMemberId")
-  case class MyIdWrap(override val value:MyId=null)  extends PersistentStateItem[MyId](value,"MyId")
-  case class RaftMembershipWrap(override val value:RaftMembership=null)  extends PersistentStateItem[RaftMembership](value,"RaftMembership")
-  case class TermInfoWrap(override val value:TermInfo=null)  extends PersistentStateItem[TermInfo](value,"TermInfo")
-  case class LeaderCommitIndexWrap(override val value:LeaderCommitIndex= -1)  extends PersistentStateItem[LeaderCommitIndex](value,"LeaderCommitIndex")
-  case class LastAppliedIndexWrap(override val value:LastAppliedIndex= -1)  extends PersistentStateItem[LastAppliedIndex](value,"LastAppliedIndex")
-  case class LastAppendedIndexWrap(override val value:LastAppendedIndex= -1)  extends PersistentStateItem[LastAppendedIndex](value,"LastAppendedIndex")
 
-  def createMyId(myId:MyId)=new PersistentState(myId=MyIdWrap(myId))
+
+  type PersistentStateDBKey=String
+  val RaftMemberIdDBKey:PersistentStateDBKey="RaftMemberId"
+  val MyIdDBKey:PersistentStateDBKey= "MyId"
+  val RaftMembershipDBKey:PersistentStateDBKey= "RaftMembership"
+  val TermInfoDBKey:PersistentStateDBKey= "TermInfo"
+  val LeaderCommitIndexDBKey:PersistentStateDBKey= "LeaderCommitIndex"
+  val LastAppliedIndexDBKey:PersistentStateDBKey="LastAppliedIndex"
+  val LastAppendedIndexDBKey:PersistentStateDBKey= "LastAppendedIndex"
 
 
 }
 import PersistentState._
 class PersistentState(
-  val raftMemberId:RaftMemberIdWrap=null ,
-  val myId:MyIdWrap =null,
-  val raftMembership:RaftMembershipWrap =null ,
-  val termInfo: TermInfoWrap =null ,
-  val leaderCommitIndex: LeaderCommitIndexWrap =null,
-  val lastAppliedIndex: LastAppliedIndexWrap =null,
-  val lastAppendedIndex: LastAppendedIndexWrap =null
+  val raftMemberId:RaftMemberId=null ,
+  val myId:MyId =null,
+  val raftMembership:RaftMembership =null ,
+  val termInfo: TermInfo =null ,
+  val leaderCommitIndex: LeaderCommitIndex = -1,
+  val lastAppliedIndex: LastAppliedIndex = -1,
+  val lastAppendedIndex: LastAppendedIndex = -1
 )
 
 
