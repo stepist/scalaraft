e6777689b0bebf0dd2e85181e6578d3a65fe5a30
stepist <stepist.kjm@gmail.com>
2013-12-21 22:40:52 +0900

    extracted  term checking routine.

--- src/main/scala/jkm/cineclub/raft/RPCHandler.scala
+++ src/main/scala/jkm/cineclub/raft/RPCHandler.scala
@@ -7,14 +7,18 @@ package jkm.cineclub.raft
  * Time: 10:04 PM
  * To change this template use File | Settings | File Templates.
  */
- /*
+
 import PersistentState._
 import jkm.cineclub.raft.DB.{PersistentStateDB, LogEntryDB}
 import LogEntryDB._
 import akka.actor._
 import scala.concurrent.duration._
 import RPCHandler._
-import CurrentState._
+import CurrentValues._
+import scala.concurrent.Await
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
 
 class RPCHandler extends Actor{
 
@@ -37,11 +41,22 @@ class RPCHandler extends Actor{
 
   def compareToLocalLog(lastLogIndex:Long  ,lastLogTerm:Long):Boolean
 
+  val raftMemeberStateManager:ActorRef=null
+  val currentValues:CurrentValues=null
+  import currentValues._
+  import RaftMemeberStateManager._
+
   def receive = {
+
+
+    case ResetTimeout => resetTimeout
+
 
     case RequestVoteRPC(term,candidateId ,lastLogIndex  ,lastLogTerm ) => {
       if (term< currentTerm) {sender ! RequestVoteRPCResult(currentTerm,false)}
       else {
+
+
         if (term> currentTerm ) {
           persistentStateDB.putState(TermInfoDBKey,TermInfo(term,votedFor))
           currentTerm=term
@@ -49,6 +64,8 @@ class RPCHandler extends Actor{
 
         if ( currentState == MemberState.Candidate | currentState == MemberState.Leader) stepDown
 
+
+        //From now, It's in follower State
 
         if ( (votedFor==null | votedFor==candidateId) & compareToLocalLog(lastLogIndex  ,lastLogTerm ) ) {
 
@@ -62,6 +79,37 @@ class RPCHandler extends Actor{
           sender ! RequestVoteRPCResult(currentTerm,false)
         }
 
+      }
+
+      term match {
+        case a if a<currentTerm => sender ! RequestVoteRPCResult(currentTerm,false)
+        case _ => {
+          implicit val timeout = Timeout(5 seconds)
+
+          val future = raftMemeberStateManager ? CheckTerm(term)
+          val result=Await.result(future, timeout.duration).asInstanceOf[CheckTermResult]
+
+          if ( memberState == MemberState.Candidate | memberState == MemberState.Leader) {
+            val future = raftMemeberStateManager ? StepDown()
+            val result=Await.result(future, timeout.duration).asInstanceOf[StepDownResult]
+          }
+
+
+          //From now, It's in follower State
+
+          if ( (votedFor==null | votedFor==candidateId) & compareToLocalLog(lastLogIndex  ,lastLogTerm ) ) {
+
+            persistentStateDB.putState(TermInfoDBKey,TermInfo(currentTerm,candidateId))
+            votedFor=candidateId
+
+            sender ! RequestVoteRPCResult(currentTerm,true)
+            resetTimeout
+
+          }else{
+            sender ! RequestVoteRPCResult(currentTerm,false)
+          }
+
+        }
       }
 
 
@@ -95,6 +143,20 @@ class RPCHandler extends Actor{
 
       }
 
+      term match {
+        case a if a < currentTerm => sender ! RequestVoteRPCResult(currentTerm,false)
+        case a if a>=0  => {
+          implicit val timeout = Timeout(5 seconds)
+
+          val future = raftMemeberStateManager ? CheckTerm(term)
+          val result=Await.result(future, timeout.duration).asInstanceOf[CheckTermResult]
+
+          if ( memberState == MemberState.Candidate | memberState == MemberState.Leader) {
+            val future = raftMemeberStateManager ? StepDown()
+            val result=Await.result(future, timeout.duration).asInstanceOf[StepDownResult]
+          }
+      }
+
 
 
 
@@ -113,5 +175,43 @@ object RPCHandler {
   case class  AppendEntriesRPC(term:Long , leaderId:RaftMemberId,prevLogIndex:Long, prevLogTerm:Long , entries: List[LogEntry] ,commitIndex:Long )
   case class  AppendEntriesRPCResult(term:Long, success:Boolean)
 
+  case class ResetTimeout()
+
 }
-   */
+
+
+class RaftMemeberStateManager extends Actor {
+  import RaftMemeberStateManager._
+
+  val persistentStateDB:PersistentStateDB=null
+  val currentValues:CurrentValues=null
+  import currentValues._
+
+  def stepDown
+
+  def receive = {
+    case CheckTerm(term) => {
+      var update=false
+      if (term> currentTerm ) {
+        persistentStateDB.putState(TermInfoDBKey,TermInfo(term,votedFor))
+        currentTerm=term
+        update=true
+      }
+      sender ! CheckTermResult(update)
+    }
+
+    case StepDown => {
+
+    }
+  }
+}
+
+object RaftMemeberStateManager {
+  case class CheckTerm(term:Long)
+  case class CheckTermResult(updated:Boolean)
+
+  case class StepDown()
+  case class StepDownResult()
+
+
+}
